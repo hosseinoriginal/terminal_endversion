f9f2213c52fbe2f1e51d26a22562e85b4f5623f8
hosseinoriginal <100521889+hosseinoriginal@users.noreply.github.com>
2024-02-02 23:17:57 +0330

    Update main.dart
    11:17 pm

--- main.dart
+++ main.dart
@@ -27,17 +27,29 @@ class Terminal {
   List<Trip> trips = [];
 
   int income = 0;
+  int cancelReservation = 0;
+  int cancelBuy = 0;
+  int emptyChair = 0;
 
 //-Void CreateBus
   void createBus() {
     stdout.write('Bus name: ');
     String? nameBus = stdin.readLineSync();
+    if (nameBus == '0') {
+      return;
+    }
     stdout.write('Bus type (1.Vip, 2.Normal): ');
     String? typeBus = stdin.readLineSync();
     if (typeBus == '1') {
+      if (typeBus == '0') {
+        return;
+      }
       buses.add(Bus(nameBus!, 'Vip'));
       print('Bus create. ✔');
     } else if (typeBus == '2') {
+      if (typeBus == '0') {
+        return;
+      }
       buses.add(Bus(nameBus!, 'Normal'));
       print('Bus create. ✔');
     } else {
@@ -48,57 +60,76 @@ class Terminal {
 
 //-Void CreateTrip
   void createTrip() {
-    if (buses.isEmpty) {
-      print('List bus is empty.!');
-      return;
+    try {
+      if (buses.isEmpty) {
+        print('List bus is empty.!');
+        return;
+      }
+      stdout.write('Choose bus type (1.Vip, 2.Normal): ');
+      String? busType = stdin.readLineSync();
+      if (busType == '0') {
+        return;
+      }
+      if (busType == null || busType.isEmpty) {
+        print('Invalid input.!');
+        return;
+      }
+      List<Bus> filterBus;
+      if (busType == '1') {
+        filterBus = buses.where((bus) => bus.type == 'Vip').toList();
+      } else if (busType == '2') {
+        filterBus = buses.where((bus) => bus.type == 'Normal').toList();
+      } else {
+        print('Invalid input.!');
+        return;
+      }
+      if (filterBus.isEmpty) {
+        print('No buses available for the selected type.!');
+        return;
+      }
+      print('Select a bus: ');
+      for (int i = 0; i < filterBus.length; i++) {
+        print(
+            '${i + 1}. Name: ${filterBus[i].name} - Type: ${filterBus[i].type}');
+      }
+      String? indexBus = stdin.readLineSync();
+      if (indexBus == '0') {
+        return;
+      }
+      if (indexBus == null || indexBus.isEmpty) {
+        print('Invalid input.!');
+        return;
+      }
+      int index = int.parse(indexBus) - 1;
+      if (index < 0 || index == null || index >= filterBus.length) {
+        print('Invalid input.!');
+        return;
+      }
+      stdout.write('Enter origin: ');
+      String? origin = stdin.readLineSync();
+      if (origin == '0') {
+        return;
+      }
+      stdout.write('Enter destination: ');
+      String? destination = stdin.readLineSync();
+      if (destination == '0') {
+        return;
+      }
+      stdout.write('Enter price: ');
+      String? priceInput = stdin.readLineSync();
+      if (priceInput == '0') {
+        return;
+      }
+      int? price = int.tryParse(priceInput!);
+      if (price == null) {
+        print('Invalid input.!');
+        return;
+      }
+      trips.add(Trip(origin!, destination!, price, buses[index]));
+      print('Trip created. ✔');
+    } catch (e) {
+      print('Input invalid!');
     }
-    stdout.write('Choose bus type (1.Vip, 2.Normal): ');
-    String? busType = stdin.readLineSync();
-    if (busType == null || busType.isEmpty) {
-      print('Invalid input.!');
-      return;
-    }
-    List<Bus> filterBus;
-    if (busType == '1') {
-      filterBus = buses.where((bus) => bus.type == 'Vip').toList();
-    } else if (busType == '2') {
-      filterBus = buses.where((bus) => bus.type == 'Normal').toList();
-    } else {
-      print('Invalid input.!');
-      return;
-    }
-    if (filterBus.isEmpty) {
-      print('No buses available for the selected type.!');
-      return;
-    }
-    print('Select a bus: ');
-    for (int i = 0; i < filterBus.length; i++) {
-      print(
-          '${i + 1}. Name: ${filterBus[i].name} - Type: ${filterBus[i].type}');
-    }
-    String? indexBus = stdin.readLineSync();
-    if (indexBus == null || indexBus.isEmpty) {
-      print('Invalid input.!');
-      return;
-    }
-    int index = int.parse(indexBus) - 1;
-    if (index < 0 || index == null || index >= filterBus.length) {
-      print('Invalid input.!');
-      return;
-    }
-    stdout.write('Enter origin: ');
-    String? origin = stdin.readLineSync();
-    stdout.write('Enter destination: ');
-    String? destination = stdin.readLineSync();
-    stdout.write('Enter price: ');
-    String? priceInput = stdin.readLineSync();
-    int? price = int.tryParse(priceInput!);
-    if (price == null) {
-      print('Invalid input.!');
-      return;
-    }
-    trips.add(Trip(origin!, destination!, price, buses[index]));
-    print('Trip created. ✔');
   }
 
 //-Void DisplayTrips
@@ -116,145 +147,186 @@ class Terminal {
 
 //Void DisplaySeat
   void showSeats(Bus bus) {
-    print('Available seats for ${bus.name} (${bus.type} bus): ');
-    for (int i = 0; i < bus.chair.length; i += 2) {
-      print('${bus.chair[i]} ${bus.chair[i + 1]}     ');
+    try {
+      print('Available seats for ${bus.name} (${bus.type} bus): ');
+      for (int i = 0; i < bus.chair.length; i += 2) {
+        print('${bus.chair[i]} ${bus.chair[i + 1]}     ');
+      }
+    } catch (e) {
+      print('Input invalid!');
     }
   }
 
 //-Void BookTicket
   void bookTicket() {
-    if (trips.isEmpty) {
-      print('No trips available for booking.!');
-      return;
-    }
-    print('Select a trip to book a ticket: ');
-    displayTrips();
-    String? indexTrip = stdin.readLineSync();
-    if (indexTrip == null || indexTrip.isEmpty) {
-      print('Invalid input.!');
-      return;
-    }
-    int index = int.parse(indexTrip) - 1;
-    if (index < 0 || index == null || index >= trips.length) {
-      print('Invalid input.!');
-      return;
-    }
-    Bus bus = buses[index];
-    showSeats(bus);
-    stdout.write('Enter the seat number you want to book: ');
-    String? seatNumber = stdin.readLineSync();
-    if (seatNumber == null ||
-        !bus.chair.contains(seatNumber) ||
-        bus.chair[bus.chair.indexOf(seatNumber)] == 'rr') {
-      print('Invalid seat number.!');
-      return;
-    }
-    bus.chair[bus.chair.indexOf(seatNumber)] = 'rr';
+    try {
+      if (trips.isEmpty) {
+        print('No trips available for booking.!');
+        return;
+      }
+      print('Select a trip to book a ticket: ');
+      displayTrips();
+      String? indexTrip = stdin.readLineSync();
+      if (indexTrip == '0') {
+        return;
+      }
+      if (indexTrip == null || indexTrip.isEmpty) {
+        print('Invalid input.!');
+        return;
+      }
+      int index = int.parse(indexTrip) - 1;
+      if (index < 0 || index == null || index >= trips.length) {
+        print('Invalid input.!');
+        return;
+      }
+      Bus bus = buses[index];
+      showSeats(bus);
+      stdout.write('Enter the seat number you want to book: ');
+      String? seatNumber = stdin.readLineSync();
+      if (seatNumber == '0') {
+        return;
+      }
+      if (seatNumber == null ||
+          !bus.chair.contains(seatNumber) ||
+          bus.chair[bus.chair.indexOf(seatNumber)] == 'rr') {
+        print('Invalid seat number.!');
+        return;
+      }
+      bus.chair[bus.chair.indexOf(seatNumber)] = 'rr';
 
-    //30% of the ticket price
-    double ticketPrice = 0.3 * trips[index].price.toDouble();
-    print('You need to pay ${ticketPrice.toInt()} T for the reservation.');
+      //30% of the ticket price
+      double ticketPrice = 0.3 * trips[index].price.toDouble();
+      print('You need to pay ${ticketPrice.toInt()} T for the reservation.');
 
-    stdout.write('Enter the amount you want to pay: ');
-    String? paymentInput = stdin.readLineSync();
-    int? payment = int.tryParse(paymentInput!);
-    if (payment == null || payment != ticketPrice) {
-      print('Invalid payment amount. Booking failed.');
-      bus.chair[bus.chair.indexOf(seatNumber)] = seatNumber;
-      return;
+      stdout.write('Enter the amount you want to pay: ');
+      String? paymentInput = stdin.readLineSync();
+      if (paymentInput == '0') {
+        return;
+      }
+      int? payment = int.tryParse(paymentInput!);
+      if (payment == null || payment != ticketPrice) {
+        print('Invalid payment amount. Booking failed.');
+        bus.chair[bus.chair.indexOf(seatNumber)] = seatNumber;
+        return;
+      }
+
+      // income += payment;
+      print('Ticket booked successfully. ✔');
+      showSeats(bus);
+    } catch (e) {
+      print('Input invalid!');
     }
-
-    income += payment;
-    print('Ticket booked successfully. ✔');
-    showSeats(bus);
   }
 
 //-Void BuyTicket
   void buyTicket() {
-    if (trips.isEmpty) {
-      print('There are no trips available for purchase.!');
-      return;
-    }
-    print('Choose a trip to buy a ticket: ');
-    displayTrips();
-    String? indexTrip = stdin.readLineSync();
-    if (indexTrip == null || indexTrip.isEmpty) {
-      print('Invalid input.!');
-      return;
-    }
-    int index = int.parse(indexTrip) - 1;
-    if (index < 0 || index >= trips.length) {
-      print('Invalid input.!');
-      return;
-    }
-    Bus bus = buses[index];
-    showSeats(bus);
-    stdout.write('Enter the number of the seat you want to buy: ');
-    String? seatNumber = stdin.readLineSync();
-    if (seatNumber == null ||
-        !bus.chair.contains(seatNumber) ||
-        bus.chair[bus.chair.indexOf(seatNumber)] == 'bb') {
-      print('Invalid seat number.!');
-      return;
-    }
-    bus.chair[bus.chair.indexOf(seatNumber)] = 'bb';
+    try {
+      if (trips.isEmpty) {
+        print('There are no trips available for purchase.!');
+        return;
+      }
+      print('Choose a trip to buy a ticket: ');
+      displayTrips();
+      String? indexTrip = stdin.readLineSync();
+      if (indexTrip == '0') {
+        return;
+      }
+      if (indexTrip == null || indexTrip.isEmpty) {
+        print('Invalid input.!');
+        return;
+      }
+      int index = int.parse(indexTrip) - 1;
+      if (index < 0 || index >= trips.length) {
+        print('Invalid input.!');
+        return;
+      }
+      Bus bus = buses[index];
+      showSeats(bus);
+      stdout.write('Enter the number of the seat you want to buy: ');
+      String? seatNumber = stdin.readLineSync();
+      if (seatNumber == '0') {
+        return;
+      }
+      if (seatNumber == null ||
+          !bus.chair.contains(seatNumber) ||
+          bus.chair[bus.chair.indexOf(seatNumber)] == 'bb') {
+        print('Invalid seat number.!');
+        return;
+      }
+      bus.chair[bus.chair.indexOf(seatNumber)] = 'bb';
 
-    //100% of the ticket price
-    int totalPrice = trips[index].price;
-    print('Total price for the ticket: ${totalPrice} T');
+      //100% of the ticket price
+      int totalPrice = trips[index].price;
+      print('Total price for the ticket: ${totalPrice} T');
 
-    stdout.write('Enter the amount you want to pay: ');
-    String? paymentInput = stdin.readLineSync();
-    int? payment = int.tryParse(paymentInput!);
-    if (payment == null || payment != totalPrice) {
-      print('Invalid payment amount. Purchase failed.');
-      bus.chair[bus.chair.indexOf(seatNumber)] = seatNumber;
-      return;
+      stdout.write('Enter the amount you want to pay: ');
+      String? paymentInput = stdin.readLineSync();
+      if (paymentInput == '0') {
+        return;
+      }
+      int? payment = int.tryParse(paymentInput!);
+      if (payment == null || payment != totalPrice) {
+        print('Invalid payment amount. Purchase failed.');
+        bus.chair[bus.chair.indexOf(seatNumber)] = seatNumber;
+        return;
+      }
+
+      // income += payment;
+      print('The ticket was successfully purchased. ✔');
+      showSeats(bus);
+    } catch (e) {
+      print('Input invalid!');
     }
-
-    income += payment;
-    print('The ticket was successfully purchased. ✔');
-    showSeats(bus);
   }
 
 //-Void CancelTicket
   void cancelTicket() {
-    if (trips.isEmpty) {
-      print('List trip is empty.!');
-      return;
+    try {
+      if (trips.isEmpty) {
+        print('List trip is empty.!');
+        return;
+      }
+      print('Choose a trip to cancel a ticket: ');
+      displayTrips();
+      String? indexTrip = stdin.readLineSync();
+      if (indexTrip == '0') {
+        return;
+      }
+      if (indexTrip == null || indexTrip.isEmpty) {
+        print('Invalid input.!');
+        return;
+      }
+      int index = int.parse(indexTrip) - 1;
+      if (index < 0 || index >= trips.length) {
+        print('Invalid input.!');
+        return;
+      }
+      Bus bus = buses[index];
+      showSeats(bus);
+      stdout.write('Enter the number of the seat you want to cancel: ');
+      String? seatNumber = stdin.readLineSync();
+      if (seatNumber == '0') {
+        return;
+      }
+      int chairNum = int.parse(seatNumber!);
+      if (seatNumber.isEmpty ||
+          seatNumber == null ||
+          bus.chair.contains(seatNumber)) {
+        print('Invalid seat number.!');
+        return;
+      }
+      int seatIndex = chairNum - 1;
+
+      if (bus.chair[seatIndex] == 'rr' || bus.chair[seatIndex] == 'bb') {
+        bus.chair[seatIndex] = seatNumber;
+        print('The ticket was successfully cancelled. ✔');
+      } else {
+        print('This seat was not previously reserved or purchased.');
+      }
+      showSeats(bus);
+    } catch (e) {
+      print('Input invalid!');
     }
-    print('Choose a trip to cancel a ticket: ');
-    displayTrips();
-    String? indexTrip = stdin.readLineSync();
-    if (indexTrip == null || indexTrip.isEmpty) {
-      print('Invalid input.!');
-      return;
-    }
-    int index = int.parse(indexTrip) - 1;
-    if (index < 0 || index >= trips.length) {
-      print('Invalid input.!');
-      return;
-    }
-    Bus bus = buses[index];
-    showSeats(bus);
-    stdout.write('Enter the number of the seat you want to cancel: ');
-    String? seatNumber = stdin.readLineSync();
-    int chairNum = int.parse(seatNumber!);
-    if (seatNumber.isEmpty ||
-        seatNumber == null ||
-        bus.chair.contains(seatNumber)) {
-      print('Invalid seat number.!');
-      return;
-    }
-    int seatIndex = chairNum - 1;
-    if (bus.chair[seatIndex] == 'rr' || bus.chair[seatIndex] == 'bb') {
-      bus.chair[seatIndex] = seatNumber;
-      print('The ticket was successfully cancelled. ✔');
-    } else {
-      print('This seat was not previously reserved or purchased.');
-    }
-    showSeats(bus);
   }
 
 //-Void TripReport
@@ -266,6 +338,9 @@ class Terminal {
     print('Choose a trip to report: ');
     displayTrips();
     String? indexTrip = stdin.readLineSync();
+    if (indexTrip == '0') {
+      return;
+    }
     if (indexTrip == null || indexTrip.isEmpty) {
       print('Invalid input.!');
       return;
@@ -276,21 +351,8 @@ class Terminal {
       return;
     }
 
-    int cancelReservation = 0;
-    int cancelBuy = 0;
-    int emptyChair = 0;
-
     for (int i = 0; i < trips[index].bus.chair.length; i++) {
-      bool isCanceled = false;
-      if (trips[index].bus.chair[i] == 'rr') {
-        cancelBuy++;
-        isCanceled = true;
-      } else if (trips[index].bus.chair[i] == 'bb') {
-        cancelReservation++;
-        isCanceled = true;
-      }
-      if (!isCanceled &&
-          trips[index].bus.chair[i] != 'rr' &&
+      if (trips[index].bus.chair[i] != 'rr' &&
           trips[index].bus.chair[i] != 'bb') {
         emptyChair++;
       }
